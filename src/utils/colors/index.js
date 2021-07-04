44e3a388648bb0a0cb041d69ac432773ce523d9e
Adhri14 <adhri.adly@gmail.com>
2021-07-04 18:23:31 +0800

    add icons add image add fonts add colors

---
+++ src/utils/colors/index.js
@@ -0,0 +1,7 @@
+export const colors = {
+  white: '#FFFFFF',
+  black: '#1A1A1A',
+  black2: '#252525',
+  grey: '#D5D5E1',
+  yellow: '#FEBE02',
+};
---
+++ src/utils/fonts/index.js
@@ -0,0 +1,6 @@
+export const fonts = {
+  bold: 'Poppins-Bold',
+  semiBold: 'Poppins-semibold',
+  medium: 'Poppins-medium',
+  regular: 'Poppins-Regular',
+};
